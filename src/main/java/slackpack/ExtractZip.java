fc4e479c7902232e31ffcb7a2fb6a0baad35e975
Francesco Davide Lapenta <lapenta.d@gmail.com>
2018-04-27 17:25:14 +0200

    Aggiunta classe ExtractZip

---
+++ src/main/java/slackpack/ExtractZip.java
@@ -0,0 +1,35 @@
+package slackpack;
+
+import net.lingala.zip4j.exception.ZipException;
+
+import net.lingala.zip4j.core.ZipFile;
+
+import java.io.File;
+
+
+
+public class ExtractZip {
+	
+
+	public static void unzip(String source) {
+
+		String destination = source.replace(".zip", "");
+		destination = destination.replace("\\", File.separator);
+		String password = "";
+
+		try {
+			ZipFile zipFile = new ZipFile(source);
+			if (zipFile.isEncrypted()) {
+				zipFile.setPassword(password);
+			}
+			zipFile.extractAll(destination);
+			System.out.println("File estratto in ");
+			System.out.println(destination);
+			System.out.println("\n");
+		// 	StampaHelp.stampaHelp();
+		} catch (ZipException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
