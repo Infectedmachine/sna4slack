cbdd54d989a253a51407771dbcc8646de87fde53
Infectedmachine <nazcheck@gmail.com>
2018-04-27 17:12:47 +0200

    aggiunto array member

---
+++ src/main/java/slackpack/ArrayMember.java
@@ -0,0 +1,54 @@
+package slackpack;
+import slackpack.Member;
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+
+import slackpack.WSParser;
+
+public class ArrayMember {
+	
+	ArrayList<Member> members; 
+	
+	public ArrayMember() {
+		
+		this.members = null; 
+	}
+	
+	public ArrayMember(String filedir) {
+		
+		WSParser parser = new WSParser(filedir);
+		this.members = new ArrayList<Member>(); 
+		
+		for (Object obj : parser.Array()) {
+			JSONObject jobj = (JSONObject) obj;
+			Member mobj = new Member();  
+			mobj.setRealName((String) jobj.get("real_name"));
+			mobj.setName((String) jobj.get("name"));
+			mobj.setID((String)jobj.get("id"));
+			members.add(mobj);
+		}
+	}
+	
+	public ArrayList<Member> getArray() {
+		
+		return this.members; 
+	}
+	
+	public void setArray(ArrayList<Member> marr) {
+		
+		this.members = marr; 
+	}
+	
+	public void printArray() {
+		
+		for (Object obj : this.members) {
+			Member mobj = (Member) obj; 
+			System.out.println(mobj.getRealName()); 
+			System.out.println(mobj.getName()); 
+			System.out.println(mobj.getID());
+		}
+	}
+	
+
+}
