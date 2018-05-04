a1a1621a9edc2692bde2be0d6c502c7fa8846296
Francesco Davide Lapenta <lapenta.d@gmail.com>
2018-05-04 18:44:20 +0200

    Aggiunto Costruttore

--- src/main/java/slackpack/ArrayMentions.java
+++ src/main/java/slackpack/ArrayMentions.java
@@ -1,13 +1,41 @@
 package slackpack;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.simple.JSONObject; 
 
 public class ArrayMentions {
 	
 	ArrayList<Mention> mentions; 
 	
-	public ArrayMentions() {
-		
-		//CONSTRUCTOR
+
+	public ArrayMentions(String filedir) {	// CONSTRUCTOR
+		WSParser parser = new WSParser(filedir);
+		this.mentions = new ArrayList<Mention>();
+		for (Object obj : parser.Array()) {
+			JSONObject jobj = (JSONObject) obj;
+			Mention mobj = new Mention();
+			String text = (String) jobj.get("text");
+			String user = (String) jobj.get("user");
+			String sub_type = (String) jobj.get("subtype");
+			
+			ArrayList<String> tmp = new ArrayList<String>();
+			
+			mobj.setFROM(user); //aggiunge l'autore del messaggio
+			
+			if (sub_type == null) {	// significa che non è un messaggio di invio file
+				Pattern findMention = Pattern.compile("<@.+?>");  // trova tutto ciò che c'è tra <@ >
+				Matcher matcher = findMention.matcher(text);
+				while (matcher.find()) {				
+					tmp.add(matcher.group().subSequence(2, matcher.group().length()-1).toString());	
+					mobj.setTO(tmp);				
+				}
+				mentions.add(mobj);
+			}
+			
+			
+		}
 		
 	}
 	
@@ -23,7 +51,20 @@ public class ArrayMentions {
 	
 	public void printMentions() {
 		
-		//SET OF INSTRUCTIONS TO PRINT ARRAY'S CONTENT
+		for ( Object obj : this.mentions) {
+			Mention mobj = (Mention) obj; 
+			System.out.println(mobj.getFROM());
+			System.out.println(mobj.getTO());
+			
+		}	
 	}
+	
+	/*
+	 * @REMEBER TO DELETE
+	public static void main(String[] args) {
+		ArrayMentions Arr = new ArrayMentions("C:\\Users\\Davide\\Downloads\\ingsw1718\\general\\2018-04-18.json");
+		Arr.printMentions();
+	}
+	 */
 
 }
