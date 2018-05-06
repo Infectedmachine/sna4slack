95efa778f744851ee55f781ffcb1d4e86da4fdd6
Francesco Davide Lapenta <lapenta.d@gmail.com>
2018-05-06 20:50:17 +0200

    Aggiunto Metodo di stampa e Update Costruttore - ArrayMentions

--- src/main/java/slackpack/ArrayMentions.java
+++ src/main/java/slackpack/ArrayMentions.java
@@ -11,31 +11,36 @@ public class ArrayMentions {
 	ArrayList<Mention> mentions;
 	
 	// CONSTRUCTOR
-	public ArrayMentions(String filedir) { 
+	public ArrayMentions(String filedir, ArrayMember members) { 
 		
 		WSParser parser = new WSParser(filedir);
 		this.mentions = new ArrayList<Mention>();
+		
 		for (Object obj : parser.Array()) {
 			JSONObject jobj = (JSONObject) obj;
 			Mention mobj = new Mention();
 			String text = (String) jobj.get("text");
 			String user = (String) jobj.get("user");
 			String sub_type = (String) jobj.get("subtype");
-			
-			mobj.setFROM(user);	// It adds the author of the message
-			
+	
 			ArrayList<String> tmp = new ArrayList<String>();
+
 			if (sub_type == null) { // It means the message it's not the type "file_share"
 				Pattern findMention = Pattern.compile("<@.+?>");
 				Matcher matcher = findMention.matcher(text);	// It finds every occurrences of "<@ >"
-				while (matcher.find()) {
-					tmp.add(matcher.group().subSequence(2, matcher.group().length() - 1).toString());	// It adds a trimmed string(without "<@" ">") to a tmp list   
-					mobj.setTO(tmp);
+				while (matcher.find()) {	// true when a mention is found in a text				
+					tmp.add(matcher.group().subSequence(2, matcher.group().length() - 1).toString());	// It adds a trimmed string(without "<@" ">") to a tmp list
+					mobj.setTO((mobj.getNameTO(tmp,members.getArray())));		
 				}
-			mentions.add(mobj);
+			
+				if (!(mobj.getTO()==null)) {			
+					mobj.setFROM(mobj.getNameFROM(user,members.getArray()));	// It adds the author of the message if he mentioned someone
+					mentions.add(mobj);
+				}
 			}
-
 		}
+
+	
 
 	}
 
@@ -50,13 +55,20 @@ public class ArrayMentions {
 	}
 
 	public void printMentions() {
-
+		
 		for (Object obj : this.mentions) {
 			Mention mobj = (Mention) obj;
-			System.out.println(mobj.getFROM());
-			System.out.println(mobj.getTO());
+			System.out.println("FROM : " + mobj.getFROM());		
+			System.out.println("TO : " + mobj.getTO());
+			System.out.println("\n");
 
 		}
 	}
+	
+	public boolean checkDouble() {
+			
+		return false;
+	}
+
 
 }
