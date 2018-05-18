68d1f1422d760f32437c46bbfd5a9a106c0643ba
Giuseppe Angelini <aleningi@gmail.com>
2018-05-18 20:38:53 +0200

    Modifica controllo nella classe CommandLineReader alla riga 81

--- src/main/java/slackpack/CommandLineReader.java
+++ src/main/java/slackpack/CommandLineReader.java
@@ -78,7 +78,7 @@ public class CommandLineReader {
 				this.setChannelName(inputcommand[3]);
 			} else
 				if (inputcommand.length == 3){
-					if (this.getInputCommand().equals("@m") || this.getInputCommand().equals("-cm"))
+					if (this.getInputCommand().matches("-cm|@m|@mw"))
 						this.setChannelName(inputcommand[2]);
 					else
 						this.setUsername(inputcommand[2]);
