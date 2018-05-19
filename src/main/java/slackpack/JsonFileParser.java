11cfda43ba1252e4c2a493e70d6faece2bd07c49
Francesco Davide Lapenta <lapenta.d@gmail.com>
2018-05-19 18:53:17 +0200

    Risolto bug DM_DEFAULT_ENCODING - Aggiunto encoding UTF-8

--- src/main/java/slackpack/JsonFileParser.java
+++ src/main/java/slackpack/JsonFileParser.java
@@ -1,25 +1,30 @@
 package slackpack;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class JsonFileParser {
-	private JSONArray contentsarray; 
-	
+	private JSONArray contentsarray;
+
 	public JsonFileParser() {
-		this.setArray(new JSONArray()); 
+		this.setArray(new JSONArray());
 	}
 	public final void fillContentsFromJSONFileDir(String filedir) {
-		
+
 		JSONParser parser = new JSONParser();
 		try {
-			
-			this.setArray((JSONArray) parser.parse(new FileReader(filedir)));
-			
+			InputStream inputStream = new FileInputStream(filedir);
+			Reader fileReader = new InputStreamReader(inputStream, "UTF-8");
+			this.setArray((JSONArray) parser.parse(fileReader));
+
 			} catch (FileNotFoundException e) {
 				System.out.println("JSON File not found");
 			} catch (IOException e) {
@@ -29,12 +34,12 @@ public class JsonFileParser {
 			}
 
 	}
-	
+
 	public final void setArray(JSONArray jsonarray) {
-		this.contentsarray = jsonarray; 
+		this.contentsarray = jsonarray;
 	}
-	
+
 	public JSONArray getArray() {
-		return this.contentsarray; 
-	}	
+		return this.contentsarray;
+	}
 }
