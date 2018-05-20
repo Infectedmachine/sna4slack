1b17ea341290acc439473bc45bcf611a8c33f687
Francesco Davide Lapenta <lapenta.d@gmail.com>
2018-05-20 21:10:20 +0200

    Checkstyle CommandLineReader

--- src/main/java/slackpack/CommandLineReader.java
+++ src/main/java/slackpack/CommandLineReader.java
@@ -2,13 +2,38 @@ package slackpack;
 
 import java.util.HashMap;
 
+/**
+ * This class consists of the attributes and methods to read commands.
+ **/
 public class CommandLineReader {
+	/**
+	 * Private attribute workspacedir, the first parameter.
+	 **/
 	private String workspacedir;
+	/**
+	 * Private attribute inputcommand, the second parameter.
+	 **/
 	private String inputcommand;
+	/**
+	 * Private attribute channelname, the name of a specified channel.
+	 **/
 	private String channelname;
+	/**
+	 * Private attribute username, the name of a specified member.
+	 **/
 	private String username;
-	HashMap<String, String> CommandTable;
+	/**
+	 * Private attribute CommandTable, it consists in the command and their meaning.
+	 **/
+	private HashMap<String, String> commandTable;
+	/**
+	 * Public constant, the max numbers of parameters.
+	 **/
+	public static final int MAX_PARAMETERS = 4;
 
+	/**
+	 * Public constructor.
+	 **/
 	public CommandLineReader() {
 		this.setWorkspaceDir("");
 		this.setInputCommand("");
@@ -18,75 +43,139 @@ public class CommandLineReader {
 		this.setDefaultCommandTable();
 	}
 
-	public final void setCommandTable(HashMap<String, String> commandtable) {
-		this.CommandTable = commandtable;
+	/**
+	 * It sets the commandtable with the input parameter.
+	 *
+	 * @param commandtable the table to be set.
+	 *
+	 **/
+	public final void setCommandTable(final HashMap<String, String> commandtable) {
+		this.commandTable = commandtable;
 	}
 
+	/**
+	 * It sets the commandtable with the default parameter.
+	 **/
 	public final void setDefaultCommandTable() {
-		this.CommandTable.put("-m", "PRINT_MEMBERS");
-		this.CommandTable.put("-c", "PRINT_CHANNELS");
-		this.CommandTable.put("-mc", "ALL_CHANNELS_MEMBERS");
-		this.CommandTable.put("-cm", "CHANNEL_MEMBERS");
-		this.CommandTable.put("@m", "MENTIONS");
-		this.CommandTable.put("@mf", "MENTIONS_FROM");
-		this.CommandTable.put("@mt", "MENTIONS_TO");
-		this.CommandTable.put("@mw", "MENTIONS_WEIGHT");
-		this.CommandTable.put("@mfw", "MENTIONS_FROM_WEIGHT");
-		this.CommandTable.put("@mtw", "MENTIONS_TO_WEIGHT");
+		this.commandTable.put("-m", "PRINT_MEMBERS");
+		this.commandTable.put("-c", "PRINT_CHANNELS");
+		this.commandTable.put("-mc", "ALL_CHANNELS_MEMBERS");
+		this.commandTable.put("-cm", "CHANNEL_MEMBERS");
+		this.commandTable.put("@m", "MENTIONS");
+		this.commandTable.put("@mf", "MENTIONS_FROM");
+		this.commandTable.put("@mt", "MENTIONS_TO");
+		this.commandTable.put("@mw", "MENTIONS_WEIGHT");
+		this.commandTable.put("@mfw", "MENTIONS_FROM_WEIGHT");
+		this.commandTable.put("@mtw", "MENTIONS_TO_WEIGHT");
 	}
 
-	public final void setWorkspaceDir(String workspacedir) {
-		this.workspacedir = workspacedir;
+	/**
+	 * It sets the workspacedir with the input parameter.
+	 *
+	 * @param wdir the workspace path
+	 *
+	 **/
+	public final void setWorkspaceDir(final String wdir) {
+		this.workspacedir = wdir;
 	}
 
-	public final void setInputCommand(String inputcommand) {
-		this.inputcommand = inputcommand;
+	/**
+	 * It sets the inputcommand with the input parameter.
+	 *
+	 * @param icommand the command to be set
+	 *
+	 **/
+	public final void setInputCommand(final String icommand) {
+		this.inputcommand = icommand;
 	}
 
-	public final void setChannelName(String channelname) {
-		this.channelname = channelname;
+	/**
+	 * It sets the channelname with the input parameter.
+	 *
+	 * @param chname
+	 *            the channel's name to be set.
+	 *
+	 **/
+	public final void setChannelName(final String chname) {
+		this.channelname = chname;
 	}
 
-	public final void setUsername(String username) {
-		this.username = username;
+	/**
+	 * It sets the username with the input parameter.
+	 *
+	 * @param usname the name of the member
+	 *
+	 **/
+	public final void setUsername(final String usname) {
+		this.username = usname;
 	}
 
+	/**
+	 * It gets the workspace path.
+	 * @return workspacedir string
+	 **/
 	public String getWorkspaceDir() {
 		return this.workspacedir;
 	}
-
+	/**
+	 * It gets the inputcommand.
+	 * @return inputcommand string
+	 **/
 	public String getInputCommand() {
 		return this.inputcommand;
 	}
-
+	/**
+	 * It gets the channel's name.
+	 * @return channelname string
+	 **/
 	public String getChannelName() {
 		return this.channelname;
 	}
-
+	/**
+	 * It gets the username.
+	 * @return username string
+	 **/
 	public String getUsername() {
 		return this.username;
 	}
 
-	public final void fillCommandsSetFromArgs(String[] inputcommand)  throws Exception {
-		if (inputcommand.length < 2 || !isValidCommand(inputcommand[1]))
-			throw new Exception ("INVALID COMMAND");
-		else {
-			this.setWorkspaceDir(inputcommand[0]);
-			this.setInputCommand(inputcommand[1]);
-			if (inputcommand.length == 4) {
-				this.setUsername(inputcommand[2]);
-				this.setChannelName(inputcommand[3]);
+	/**
+	 * It fills the attributes depending on what the user writes.
+	 *
+	 * @param icommand the args from the main class.
+	 *
+	 * @throws Exception when the command are not valid.
+	 *
+	 **/
+	public final void fillCommandsSetFromArgs(final String[] icommand)  throws Exception {
+		if (icommand.length < 2 || !isValidCommand(icommand[1])) {
+			throw new Exception("INVALID COMMAND");
+		} else {
+			this.setWorkspaceDir(icommand[0]);
+			this.setInputCommand(icommand[1]);
+			if (icommand.length == MAX_PARAMETERS) {
+				this.setUsername(icommand[2]);
+				this.setChannelName(icommand[3]);
 			} else
-				if (inputcommand.length == 3){
-					if (this.getInputCommand().matches("-cm|@m|@mw"))
-						this.setChannelName(inputcommand[2]);
-					else
-						this.setUsername(inputcommand[2]);
+				if (icommand.length == MAX_PARAMETERS - 1) {
+					if (this.getInputCommand().matches("-cm|@m|@mw")) {
+						this.setChannelName(icommand[2]);
+					} else {
+						this.setUsername(icommand[2]);
+					}
 			}
 		}
 	}
 
-	public boolean isValidCommand(String command) {
-		return ((this.CommandTable.get(command) != null) ? true : false);
+	/**
+	 * It checks if the commands are valid.
+	 *
+	 * @param command the command to be checked.
+	 *
+	 * @return true if the commands are valid.
+	 **/
+	public boolean isValidCommand(final String command) {
+		return ((this.commandTable.get(command) != null));
 	}
 }
+
